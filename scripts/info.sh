4d15618fef17be0b181996c3a995ece65b3e434e
Max Howell <mxcl@me.com>
2025-05-06 16:04:23 -0400

    `mash info --random`

--- scripts/info.sh
+++ scripts/info.sh
@@ -2,12 +2,18 @@
 
 if [ ! "$1" ]; then
   echo "usage: mash info <program>" 1>&2
+  echo "usage: mash info <--random|-r>" 1>&2
   exit 1
 fi
 
-if [ "$1" == '--help' ]; then
+if [ "$1" = '--help' -o "$1" = '--help' ]; then
   echo "usage: mash info <program>"
+  echo "usage: mash info <--random|-r>"
   exit 0
+fi
+
+if [ "$1" == '--random' -o "$1" == '-r' ]; then
+  set -- "$(pkgx -Q | tr , '\n' | pkgx -q shuf -n1)"
 fi
 
 IFS=$',\n' read -r -a project < <(pkgx -Q "$1")
@@ -22,4 +28,9 @@ if [ "${#project[@]}" -gt 1 ]; then
   exit 1
 fi
 
-open "https://pkgx.dev/pkgs/${project[0]}/"
+if [ $(uname) = Darwin ]; then
+  open "https://pkgx.dev/pkgs/${project[0]}/"
+else
+  # TODO pkg this for pkgx
+  xdg-open "https://pkgx.dev/pkgs/${project[0]}/"
+fi
